2a423cf0a947a2ceed28d2f699737273fc4d379a
James E. Marca <jmarca@translab.its.uci.edu>
2014-02-03 15:38:07 -0800

    One of the grids no longer has records other than VDS links
    hacking out a guess at teh correct fix

--- test/test_sql_query.sql
+++ test/test_sql_query.sql
@@ -1262,8 +1262,6 @@ hpms_grid_county as (
 ,hpmsgrids_summed as (
     select cell, hpms_id, sum(clipped_length) as clipped_length
     from hpms_grid_county group by cell, hpms_id )
-select count(*) from hpmsgrids_summed;
-
 , hpms_links as (
     select distinct hpms_id
     from hpmsgrids_summed )
@@ -1273,7 +1271,8 @@ select count(*) from hpmsgrids_summed;
     join hpms.hpms_link_geom hd using (hpms_id)
     join hpms.hpms_geom hg on (hd.geo_id = hg.id)     group by hpms_id )
 , hpms_fractional as (
-    select cell,hpms_id,clipped_length,orig_length,clipped_length/orig_length as clipped_fraction
+    select cell,hpms_id,clipped_length,orig_length,clipped_length/orig_length as
+clipped_fraction
     from hpmsgrids_summed hgs
     join hpms_only htl using (hpms_id) )
 , hpms_fractional_limited as (
@@ -1284,7 +1283,6 @@ select count(*) from hpmsgrids_summed;
     from hpms_grid_county hga
     join hpms_fractional_limited hfl on (hga.hpms_id=hfl.hpms_id)
 )
-
 select cell,year,route_number,f_system, sum(aadt) as sum_aadt,
     floor(sum(aadt*sec_len_miles)) as sum_vmt,
 sum(sec_len_miles*through_lanes) as sum_lane_miles,
@@ -1295,3 +1293,16 @@ floor(sum(avg_combination*aadt*sec_len_miles/100)) as sum_combination_mt
 from hpms_joined
 group by cell,year,route_number,f_system
 order by cell,year,f_system
+
+
+
+
+-- alameda is failing
+
+with grid_cell as ( select floor(grids.i_cell) || '_'|| floor(grids.j_cell) as cell ,geom4326 from carbgrid.state4k grids where (floor(grids.i_cell) || '_'|| floor(grids.j_cell)='132_164') ) ,fips_code as ( select (regexp_matches(fips, '060*(\d*)'))[1] as fips from counties_fips where upper(name)='ALAMEDA' ), hpms_grid_county as (select grid_cell.cell,grid_cell.geom4326,      hd.id as hpms_id, year_record as year, state_code,is_metric,hd.fips,begin_lrs,end_lrs,      route_number, type_facility,f_system,gf_system, aadt,through_lanes,      lane_width, peak_parking,      speed_limit, design_speed,      perc_single_unit as pct_s_u_pk_hr,      coalesce(avg_single_unit,0.0) as avg_single_unit,      perc_combination as pct_comb_pk_hr,      coalesce(avg_combination,0.0) as avg_combination,      k_factor,dir_factor,      peak_lanes,peak_capacity,      county, locality,link_desc,from_name, to_name     ,CASE WHEN is_metric>0 THEN section_length*0.621371 ELSE section_length END as full_sec_len_miles ,hg.* ,st_length( ST_Intersection(geom4326, hg.geom)  ) as clipped_length from hpms.hpms_data hd      join hpms.hpms_link_geom hlg on (hlg.hpms_id=hd.id)      join hpms.hpms_geom hg on (hg.id=hlg.geo_id)      inner join grid_cell on ( st_intersects(geom4326,geom) )      join fips_code fc on (fc.fips = hd.fips) where section_id !~ 'FHWA*' and state_code=6 and year_record=2008 )
+,hpmsgrids_summed as (   select cell, hpms_id, sum(clipped_length) as clipped_length   from hpms_grid_county group by cell, hpms_id ) , hpms_links as (     select distinct hpms_id from hpmsgrids_summed ) , hpms_only as (     select hpms_id            ,sum(st_length(hg.geom)) as orig_length     from hpms_links hgs     join hpms.hpms_link_geom hd using (hpms_id)     join hpms.hpms_geom hg on (hd.geo_id = hg.id)     group by hpms_id ) , hpms_fractional as (     select cell,hpms_id,clipped_length,orig_length,clipped_length/orig_length as clipped_fraction     from hpmsgrids_summed hgs     join hpms_only htl using (hpms_id) ) , hpms_fractional_limited as (   select cell,hpms_id, clipped_fraction   from hpms_fractional   where clipped_fraction > 0.01 )
+, hpms_joined as (   select hga.*, clipped_fraction, clipped_fraction*full_sec_len_miles as sec_len_miles   from hpms_grid_county hga   join hpms_fractional_limited hfl on (hga.hpms_id=hfl.hpms_id) )
+select cell,year,route_number,f_system, sum(aadt) as sum_aadt,  floor(sum(aadt*sec_len_miles)) as sum_vmt, sum(sec_len_miles*through_lanes) as sum_lane_miles, floor(sum(avg_single_unit*aadt/100)) as sum_single_unit, floor(sum(avg_single_unit*aadt*sec_len_miles/100)) as sum_single_unit_mt, floor(sum(avg_combination*aadt/100)) as sum_combination,                 floor(sum(avg_combination*aadt*sec_len_miles/100)) as sum_combination_mt from hpms_joined  group by cell,year,route_number,f_system  order by cell,year,f_system
+
+-- looks fine.  only picking off I80 links, before was probably
+-- getting things in other counties?
