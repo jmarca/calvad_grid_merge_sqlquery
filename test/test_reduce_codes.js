d18f739f2c05b1a8e4abbc755a88f251efffaf88
James E. Marca <james@activimetrics.com>
2014-12-01 20:55:46 -0800

    test for reduce codes passing
    old way looked for sum_aadt, but sum_aadt is a bad idea so I dropped it
    last Jan 2014 without fixing the test.  also fixed the quantities in the
    test, so that if there are problems down the road (say if the db
    changes) then I can adjust the test

--- test/test_reduce_codes.js
+++ test/test_reduce_codes.js
@@ -6,7 +6,11 @@ var async = require('async')
 var _ = require('lodash')
 
 var reduce = require('../lib/reduce')
-var config_okay = require('../lib/config_okay')
+var config_okay = require('config_okay')
+var path = require('path')
+var rootdir = path.normalize(__dirname)
+var config_file = rootdir+'/../test.config.json'
+
 var queries = require('../lib/query_postgres')
 var get_hpms_aadt = queries.get_hpms_from_sql
 var get_detector_routes = queries.get_detector_route_nums
@@ -20,7 +24,7 @@ var test_db_unique = date.getHours()+'-'
 
 var config
 before(function(done){
-    config_okay('test.config.json',function(err,c){
+    config_okay(config_file,function(err,c){
         config ={'postgres':c.postgres
                 ,'couchdb':c.couchdb}
 
@@ -90,41 +94,35 @@ describe('post process sql queries',function(){
                                   //console.log(task.aadt_store)
                                   task.aadt_store.should.have.property('14')
                                   task.aadt_store['14']
-                                  .should.have.keys('sum_aadt'
-                                                   ,'sum_vmt'
+                                  .should.have.keys('sum_vmt'
                                                    ,'sum_lane_miles'
                                                    ,'sum_single_unit_mt'
                                                    ,'sum_combination_mt')
-                                  task.aadt_store['14'].should.have.property( 'sum_aadt').with.approximately(185496,0.1)
-                                  task.aadt_store['14'].should.have.property( 'sum_vmt').with.approximately(137844,0.1)
-                                  task.aadt_store['14'].should.have.property( 'sum_lane_miles').with.approximately(25.69,0.01)
-                                  task.aadt_store['14'].should.have.property( 'sum_single_unit_mt').with.approximately(4803,0.01)
-                                  task.aadt_store['14'].should.have.property( 'sum_combination_mt').with.approximately(1005,0.01)
+                                  task.aadt_store['14'].should.have.property( 'sum_vmt').with.approximately((93787+16001+3747),0.1)
+                                  task.aadt_store['14'].should.have.property( 'sum_lane_miles').with.approximately((17.15+2.6+.34),0.01)
+                                  task.aadt_store['14'].should.have.property( 'sum_single_unit_mt').with.approximately((3383+480+0),0.01)
+                                  task.aadt_store['14'].should.have.property( 'sum_combination_mt').with.approximately((950+0+0),0.01)
 
                                   task.aadt_store.should.have.property('16')
                                   task.aadt_store['16']
-                                  .should.have.keys('sum_aadt'
-                                                   ,'sum_vmt'
+                                  .should.have.keys('sum_vmt'
                                                    ,'sum_lane_miles'
                                                    ,'sum_single_unit_mt'
                                                    ,'sum_combination_mt')
-                                  task.aadt_store['16'].should.have.property( 'sum_aadt').with.approximately(95491,0.1)
-                                  task.aadt_store['16'].should.have.property( 'sum_vmt').with.approximately(102226,0.1)
-                                  task.aadt_store['16'].should.have.property( 'sum_lane_miles').with.approximately(32.50,0.01)
-                                  task.aadt_store.should.have.property('17')
-                                  task.aadt_store['16'].should.have.property( 'sum_single_unit_mt').with.approximately(879,0.01)
-                                  task.aadt_store['16'].should.have.property( 'sum_combination_mt').with.approximately(97,0.01)
+                                  task.aadt_store['16'].should.have.property( 'sum_vmt').with.approximately((4507+51935),0.1)
+                                  task.aadt_store['16'].should.have.property( 'sum_lane_miles').with.approximately((2.9+11.37),0.01)
+                                  task.aadt_store['16'].should.have.property( 'sum_single_unit_mt').with.approximately((90+306),0.01)
+                                  task.aadt_store['16'].should.have.property( 'sum_combination_mt').with.approximately((0+97),0.01)
 
+                                  task.aadt_store.should.have.property('17')
                                   task.aadt_store['17']
-                                  .should.have.keys('sum_aadt'
-                                                   ,'sum_vmt'
+                                  .should.have.keys('sum_vmt'
                                                    ,'sum_lane_miles'
                                                    ,'sum_single_unit_mt'
                                                    ,'sum_combination_mt')
-                                  task.aadt_store['17'].should.have.property( 'sum_aadt').with.approximately(153724,0.1)
-                                  task.aadt_store['17'].should.have.property( 'sum_vmt').with.approximately(91443,0.1)
-                                  task.aadt_store['17'].should.have.property( 'sum_lane_miles').with.approximately(54.03,0.01)
-                                  task.aadt_store['17'].should.have.property( 'sum_single_unit_mt').with.approximately(235,0.01)
+                                  task.aadt_store['17'].should.have.property( 'sum_vmt').with.approximately(61562,0.1)
+                                  task.aadt_store['17'].should.have.property( 'sum_lane_miles').with.approximately(39.82,0.01)
+                                  task.aadt_store['17'].should.have.property( 'sum_single_unit_mt').with.approximately(130,0.01)
                                   task.aadt_store['17'].should.have.property( 'sum_combination_mt').with.approximately(0,0.01)
 
                                   return done()
@@ -133,3 +131,30 @@ describe('post process sql queries',function(){
 
        })
 })
+
+/**
+ * The tests were developed as follows.  first run the query on the bare db, and you get
+ *
+  cell  | year | route_number | f_system | sum_aadt | sum_vmt |  sum_lane_miles   | sum_single_unit | sum_single_unit_mt | sum_combination | sum_combination_mt
+--------+------+--------------+----------+----------+---------+-------------------+-----------------+--------------------+-----------------+--------------------
+ 189_72 | 2009 | 101          |        2 |    29000 |   20119 |  2.77508869053543 |               0 |                  0 |               0 |                  0
+ 189_72 | 2009 | 101          |       12 |   267000 |   95487 |  4.29814414732388 |            6670 |               2581 |            8000 |               3252
+ 189_72 | 2009 |              |       14 |   151196 |   93787 |   17.150024415496 |            5098 |               3383 |            1381 |                950
+ 189_72 | 2009 | 192          |       14 |    12300 |   16001 |  2.60181767868063 |             369 |                480 |               0 |                  0
+ 189_72 | 2009 | 225          |       14 |    22000 |    3747 | 0.340672814821636 |               0 |                  0 |               0 |                  0
+ 189_72 | 2009 | 192          |       16 |     3100 |    4507 |  2.90801266914361 |              62 |                 90 |               0 |                  0
+ 189_72 | 2009 |              |       16 |    92391 |   51935 |  11.3696098361967 |            1034 |                306 |             240 |                 97
+ 189_72 | 2009 |              |       17 |   153724 |   61562 |   39.826748623163 |             547 |                130 |               0 |                  0
+(8 rows)
+
+ * I want to rip out 101, as it is tracked already.
+ *
+ * so summing, you should get
+ *
+ * f_system 2 should not exist
+ * f_system 12 should not exist
+ *
+ * the others should be unchanged.
+ *
+ *
+ */
