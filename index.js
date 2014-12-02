3367c6e2ba6484304e15048b56bbe0dfc4db3bdd
James E. Marca <james@activimetrics.com>
2014-12-01 21:43:36 -0800

    clean up package; export f_system.json stuffs

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ exports.pg_done=q.pg_done
 exports.post_process_sql_queries=r.post_process_sql_queries
 exports.apply_fractions=r.apply_fractions
 exports.reduce=r.reduce
+var f_system = require('./lib/f_system.json')
+// add an entry for detector_based "class" of highway
+f_system.detector_based='detector based hwy data'
+exports.f_system=f_system
