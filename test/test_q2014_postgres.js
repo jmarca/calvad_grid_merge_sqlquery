16516ae30d49fa721829635fe4cf2a3a25d8b839
James E. Marca <james@activimetrics.com>
2016-12-28 11:56:13 -0800

    update query to get combination, single_unit
    forgot to get these from the new, combined table.
    
    test passes for localhost hpmstest db, which has 2014 and 2015 data
    
    have not yet developed a true test with temporary data
    
    2014 data does not have truck info (no single unit, no combination) 2015
    data does have truck data, but not for ramps, etc

--- test/test_q2014_postgres.js
+++ test/test_q2014_postgres.js
@@ -20,13 +20,12 @@ before(function(done){
     return null
 })
 
-var yr = 2014
 
 describe('get_hpms_from_sql (2014)',function(){
-    it('should get data from sql, 2014 table'
+    it('should get data from sql, hpms_count_data table'
        ,function(done){
            var task={'cell_id':'189_72'
-                     ,'year':yr
+                     ,'year':2014
                      ,'options':config
                     }
            queries.get_hpms_from_sql_2014(task,function(err,cbtask){
@@ -35,13 +34,63 @@ describe('get_hpms_from_sql (2014)',function(){
                should.exist(cbtask)
                cbtask.should.have.property('accum').with.lengthOf(6);
                cbtask.accum.forEach(function(row){
-                   Object.keys(row).should.have.length(6)
+                   Object.keys(row).should.have.length(10)
                    row.should.have.property('cell','189_72')
-                   row.should.have.property('year',yr)
+                   row.should.have.property('year',2014)
                    row.should.have.property('f_system')
                    row.f_system.should.not.eql('NULL')
                    row.should.have.property('sum_aadt')
                    row.should.have.property('sum_vmt')
+                   row.should.have.property('sum_single_unit')
+                   row.should.have.property('sum_combination')
+                   row.should.have.property('sum_single_unit_mt')
+                   row.should.have.property('sum_combination_mt')
+                   row.sum_single_unit_mt.should.eql(0)
+                   row.sum_combination_mt.should.eql(0)
+               })
+               done()
+           })
+           return null
+       })
+    return null
+})
+describe('get_hpms_from_sql (2015)',function(){
+    it('should get data from sql, new hpms_count_data table'
+       ,function(done){
+           var task={'cell_id':'189_72'
+                     ,'year':2015
+                     ,'options':config
+                    }
+           queries.get_hpms_from_sql_2014(task,function(err,cbtask){
+               // err should not exist
+               should.not.exist(err)
+               should.exist(cbtask)
+               cbtask.should.have.property('accum').with.lengthOf(6);
+               cbtask.accum.forEach(function(row){
+                   Object.keys(row).should.have.length(10)
+                   row.should.have.property('cell','189_72')
+                   row.should.have.property('year',2015)
+                   row.should.have.property('f_system')
+                   row.f_system.should.not.eql('NULL')
+                   row.should.have.property('sum_aadt')
+                   row.should.have.property('sum_vmt')
+                   row.should.have.property('sum_single_unit')
+                   row.should.have.property('sum_combination')
+                   row.should.have.property('sum_single_unit_mt')
+                   row.should.have.property('sum_combination_mt')
+                   //console.log(row)
+                   if(row.f_system=='CO P' ||
+                      row.f_system=='Santa Barbara P' ||
+                      row.f_system=='SHS_101_P' ||
+                      row.f_system=='SHS_192_P' ||
+                      row.f_system=='SHS_225_P'
+                     ){
+                       row.sum_single_unit_mt.should.be.above(0)
+                       row.sum_combination_mt.should.be.above(0)
+                   }else{
+                       row.sum_single_unit_mt.should.eql(0)
+                       row.sum_combination_mt.should.eql(0)
+                   }
                })
                done()
            })
