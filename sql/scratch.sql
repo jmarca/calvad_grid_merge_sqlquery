32e697412f4619eacf8c5f3c31a82d063bcfdbf8
James E. Marca <james@activimetrics.com>
2017-01-30 14:32:56 -0800

    update queries to reflect actual 2014, 2015 data
    truck data officially released as counts, not fractions (like in HPMS
    spec, and in 2007--2009 data).  Still no lane data, so lane miles is
    zero.

--- sql/scratch.sql
+++ sql/scratch.sql
@@ -619,3 +619,76 @@ names_cities as (   select hgc.*,   case
   floor(sum(aadt*sec_len_miles)) as sum_vmt  from hpms_joined  group
   by cell,year,classify,route_number  order by
   cell,year,classify,route_number
+
+
+-- post valid 2014, 2015 data, debugging sql query
+
+with
+grid_cell as (
+  select floor(grids.i_cell) || '_'|| floor(grids.j_cell) as cell
+         ,geom4326
+  from carbgrid.state4k grids
+  where (floor(grids.i_cell) || '_'|| floor(grids.j_cell)='231_52') ),
+fips_code as (
+  select (regexp_matches(fips, '060*(\d*)'))[1] as fips from counties_fips
+  where upper(name)='ORANGE' ),
+ hpms_grid_county as (
+  select grid_cell.cell,
+         hd.segment_id as hpms_id, year_record as year,
+         state_code,fips,
+     route_id,
+     aadt,
+     section_length as full_sec_len_miles
+     ,geom as geom
+     ,st_length( ST_Intersection(geom4326, geom)  ) as clipped_length
+     ,regexp_split_to_array(route_id,E'\s*_\s*') as name_parts
+  from grid_cell
+  join hpms.hpms_count_data hd
+       on ( st_intersects(geom4326,st_transform(geom,4326)) )
+  join fips_code fc on (1=1)
+  where
+     year_record=2015 ),
+hpmsgrids_summed as (
+  select cell, hpms_id,
+         sum(clipped_length) as clipped_length
+  from hpms_grid_county   group by cell, hpms_id ),
+ hpms_links as (
+
+  select distinct hpms_id
+  from hpmsgrids_summed ),
+ hpms_only as (
+  select hpms_id
+         ,st_length(hg.geom) as orig_length
+  from hpms_links hgs
+  join hpms.hpms_count_data hg on (hg.segment_id = hgs.hpms_id) ),
+ hpms_fractional as (
+  select cell,hpms_id,clipped_length,orig_length,clipped_length/orig_length as clipped_fraction
+  from hpmsgrids_summed hgs
+  join hpms_only htl using (hpms_id) ),
+ hpms_fractional_limited as (
+  select cell,hpms_id, clipped_fraction
+  from hpms_fractional
+  where clipped_fraction > 0.01 ),
+ names_cities as (
+  select hgc.*,
+  case
+    when array_length(name_parts,1) = 4 then upper(name_parts[2])
+    when name_parts[1] ~* 'SHS' then 'SHS'
+    else name_parts[1]
+  end as city_abbrev
+  from hpms_grid_county hgc ),
+ city_join as (
+  select hgc.*,coalesce(ca.name,hgc.city_abbrev) as city_name
+  from names_cities hgc
+  left outer join geocoding.city_abbrev ca on (upper(ca.abbrev)=hgc.city_abbrev) ),
+ hpms_joined as (
+  select hga.*, clipped_fraction, clipped_fraction*full_sec_len_miles as sec_len_miles ,case
+  when array_length(name_parts,1) = 4 then city_name || ' ' || name_parts[4]
+  when name_parts[1] ~* 'SHS' then route_id
+  else name_parts[1] end as classify ,case
+  when name_parts[1] ~* 'SHS' then name_parts[2] end as route_number
+  from city_join hga
+  join hpms_fractional_limited hfl on (hga.hpms_id=hfl.hpms_id) )
+  select cell,year,classify as f_system,route_number,
+        sum(aadt) as sum_aadt,
+        floor(sum(aadt*sec_len_miles)) as sum_vmt  from hpms_joined  group by cell,year,classify,route_number  order by cell,year,classify,route_number;
